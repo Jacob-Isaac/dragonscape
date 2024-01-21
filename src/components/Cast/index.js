3915e473e47fd47f5806c3a1810a71f543f38264
Jakub Nowakowski <jacob.69.isaac@gmail.com>
2024-01-21 12:49:15 +0100

    new about us content

--- src/components/Cast/index.js
+++ src/components/Cast/index.js
@@ -11,7 +11,6 @@ import LoadingPage from "../../features/TechnicalTabs/LoadingPage";
 import ErrorPage from "../../features/TechnicalTabs/ErrorPage";
 
 const Cast = ({ selectedCast }) => {
-
   const dispatch = useDispatch();
   const theme = useSelector(selectIsLightMode);
   const ifLoading = useSelector(selectLoadingState);
@@ -28,69 +27,65 @@ const Cast = ({ selectedCast }) => {
       break;
     case "success":
       returned = (
-    <>
-    <h1>O nas</h1>
-    <br/>
-      <p>
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
-        since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book. It has survived not only five
-        centuries, but also the leap into electronic typesetting, remaining
-        essentially unchanged. It was popularised in the 1960s with the release
-        of Letraset sheets containing Lorem Ipsum passages, and more recently
-        with desktop publishing software like Aldus PageMaker including versions
-        of Lorem Ipsum.  <br/> <br/>
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
-        since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book. It has survived not only five
-        centuries, but also the leap into electronic typesetting, remaining
-        essentially unchanged. It was popularised in the 1960s with the release
-        of Letraset sheets containing Lorem Ipsum passages, and more recently
-        with desktop publishing software like Aldus PageMaker including versions
-        of Lorem Ipsum. <br/> <br/>
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
-        since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book. It has survived not only five
-        centuries, but also the leap into electronic typesetting, remaining
-        essentially unchanged. It was popularised in the 1960s with the release
-        of Letraset sheets containing Lorem Ipsum passages, and more recently
-        with desktop publishing software like Aldus PageMaker including versions
-        of Lorem Ipsum. <br/> <br/>
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
-        since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book. It has survived not only five
-        centuries, but also the leap into electronic typesetting, remaining
-        essentially unchanged. It was popularised in the 1960s with the release
-        of Letraset sheets containing Lorem Ipsum passages, and more recently
-        with desktop publishing software like Aldus PageMaker including versions
-        of Lorem Ipsum.
-      </p>
-      <TilesPersonSection>
-        {selectedCast.map((person) => (
-          <TilePerson
-            key={person.id + "_" + person.job}
-            personName={person.name}
-            personRole={person.character}
-            personId={person.id}
-            personProfilePath={person.profile_path}
-          />
-        ))}
-      </TilesPersonSection>
-    </>
-  );
-  break;
-  case "error":
-    returned = <ErrorPage />;
-    break;
-  default:
-    returned = <LoadingPage />;
-}
+        <>
+          <h1>O nas</h1>
+          <br />
+          <p>
+            Jesteśmy Dragonscape! Zajmujemy się projektowaniem logo i
+            identyfikacji wizualnej, a także tworzeniem profesjonalnych stron internetowych
+            dla różnych branż i sektorów. Naszą misją jest dostarczanie wysokiej
+            jakości usług, które spełniają oczekiwania naszych klientów i
+            pomagają im osiągnąć sukces w biznesie. <br />
+            <br />
+            Naszą firmę założyli bracia: Miłosz i Jakub, którzy mają
+            bogate doświadczenie w branży handlowej oraz IT. Miłosz jest
+            absolwentem informatyki oraz założycielem marki "Promuj Siebie".
+            Jakub jest ekspertem do spraw stosunków międzynarodowych (specjalność gospodarka i biznes międzynarodowy). 
+            Razem postanowili połączyć swoje umiejętności i zainteresowania, aby
+            stworzyć firmę, która oferuje kompleksowe rozwiązania dla potrzeb
+            internetowych. W naszym zespole pracują również inni utalentowani i
+            kreatywni ludzie, którzy dbają o każdy aspekt projektu, od analizy,
+            przez projektowanie, po wdrożenie i utrzymanie.
+            <br />
+            <br />
+            Używamy nowoczesnych technologii i narzędzi, aby zapewnić naszym
+            klientom szybkie, bezpieczne i funkcjonalne strony internetowe oraz
+            design, który przyciąga uwagę i buduje zaufanie. Nasze portfolio
+            obejmuje projekty dla różnych sektorów, takich jak gastronomia,
+            turystyka, gaming, sport, czy kultura.
+            Niezależnie od tego, czy potrzebujesz prostej wizytówki,
+            rozbudowanego sklepu internetowego, czy oryginalnego logo, jesteśmy
+            gotowi podjąć się wyzwania i dostosować się do twoich potrzeb i
+            preferencji.
+            <br />
+            <br />
+            Jeśli szukasz profesjonalnej i niezawodnej firmy, która stworzy dla
+            ciebie stronę internetową i logo, skontaktuj się z nami. Zapewniamy
+            konkurencyjne ceny, terminową realizację i gwarancję satysfakcji. Z
+            Dragonscape twoja firma zyska nowy wymiar w internecie!
+          </p>
+          <TilesPersonSection>
+            {selectedCast.map((person) => (
+              <TilePerson
+                key={person.id + "_" + person.job}
+                personName={person.name}
+                personRole={person.character}
+                personId={person.id}
+                personProfilePath={person.profile_path}
+              />
+            ))}
+          </TilesPersonSection>
+        </>
+      );
+      break;
+    case "error":
+      returned = <ErrorPage />;
+      break;
+    default:
+      returned = <LoadingPage />;
+  }
 
-return returned;
+  return returned;
 };
 
 export default Cast;
